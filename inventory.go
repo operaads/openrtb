bacde1c761077bd938dad00720fbdecb1aa17731
yuansip <yuansip@oupeng.com>
2022-12-14 17:09:18 +0800

    ADX-9477: omitempty for req.app.domain

--- inventory.go
+++ inventory.go
@@ -3,7 +3,7 @@ package openrtb
 type Inventory struct {
 	ID            string     `json:"id,omitempty"` // ID on the exchange
 	Name          string     `json:"name,omitempty"`
-	Domain        string     `json:"domain"`
+	Domain        string     `json:"domain,omitempty"`
 	Cat           []string   `json:"cat,omitempty"`           // Array of IAB content categories
 	SectionCat    []string   `json:"sectioncat,omitempty"`    // Array of IAB content categories for subsection
 	PageCat       []string   `json:"pagecat,omitempty"`       // Array of IAB content categories for page
