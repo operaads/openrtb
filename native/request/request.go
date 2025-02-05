52268d38db5a538826e7b99c108693fb895732a6
jizey <jizey@opera.com>
2025-02-05 10:03:45 +0800

    ADX-14590 remove omitempty for aurlsupport

--- native/request/request.go
+++ native/request/request.go
@@ -86,7 +86,7 @@ type RequestV12 struct {
 	PlacementCount   int               `json:"plcmtcnt,omitempty"`       // The number of identical placements in this Layout
 	Sequence         int               `json:"seq,omitempty"`            // 0 for the first ad, 1 for the second ad, and so on
 	Assets           []Asset           `json:"assets"`                   // An array of Asset Objects
-	AUrlSupport      int               `json:"aurlsupport,omitempty"`
+	AUrlSupport      int               `json:"aurlsupport"`
 	DUrlSupport      int               `json:"durlsupport,omitempty"`
 	EventTrackers    []EventTracker    `json:"eventtrackers,omitempty"`
 	Privacy          int               `json:"privacy,omitempty"`
